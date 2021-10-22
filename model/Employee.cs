f2886685d8c26e33db672cd6996b5ee5f15ace95
Julio Ochoa <julio.e.ochoa.p@gmail.com>
2021-10-22 12:57:12 -0500

    Refactoring namespace

--- model/Employee.cs
+++ model/Employee.cs
@@ -1,6 +1,7 @@
-namespace testing.model
-public employee {
-    public int Id { get; set; }
-    public int Name { get; set; }
-    public Employee()  {}
+namespace testing.model {
+    public employee {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public Employee()  {}
+    }
 }
